dd2404d3464d7a1b45a0133bfe74fa0920faab9b
Jorge B <43991910+jorgecnj94@users.noreply.github.com>
2019-03-25 16:02:30 +0100

    Final sin Tecnologias

---
+++ js/index.js
@@ -0,0 +1,17 @@
+/**
+ * La función $(document).ready() de jQuery 
+ * ejecuta este código cuando se carga totalmente
+ * la página (index.html).
+ */
+
+$(document).ready(function() {
+    draw();
+    draw1();
+    draw2();
+    draw3();
+    draw4();
+    draw5();
+    draw6();
+    draw7();
+    $.controlador.init("panel_sobre");
+});
